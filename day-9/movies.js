155bc1add5b2266ab9ba4fe0929525473ad33924
shivakumarmusani <138885221+shivakumarmusani@users.noreply.github.com>
2025-02-06 18:28:24 +0530

    add

---
+++ day-9/movies.js
@@ -0,0 +1,16 @@
+//movies.js
+const movie1 = {
+    title: "kalki",
+    time:"10AM",
+    seats: 60,
+};
+const movie2 = {
+    title: "Pushpa 2",
+    time: "2PM",
+    seats: 80,
+};
+const movie3 = {
+    title: "KGF",
+    time: "6PM",
+    seats: 90,
+};
